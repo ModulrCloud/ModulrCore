738ca2757400302143bf830a076389a68d5ccd52
VladChernenko <vladchernenko@protonmail.com>
2025-09-07 04:45:09 +0200

    Added context to stake and unstake delayed operations

--- system_contracts/delayed_transactions.go
+++ system_contracts/delayed_transactions.go
@@ -181,54 +181,109 @@ func Stake(delayedTransaction map[string]string, context string) bool {
 
 	}
 
-	poolStorage := common_functions.GetFromApprovementThreadState(poolPubKey + "(POOL)_STORAGE_POOL")
+	if context == "AT" {
 
-	if poolStorage != nil {
+		poolStorage := common_functions.GetFromApprovementThreadState(poolPubKey + "(POOL)_STORAGE_POOL")
 
-		minStake := globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.NetworkParameters.MinimalStakePerEntity
+		if poolStorage != nil {
 
-		if amount < minStake {
+			minStake := globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.NetworkParameters.MinimalStakePerEntity
 
-			return false
+			if amount < minStake {
 
-		}
-
-		if _, exists := poolStorage.Stakers[staker]; !exists {
-
-			poolStorage.Stakers[staker] = structures.Staker{
-
-				Stake: 0,
-			}
-
-		}
-
-		stakerData := poolStorage.Stakers[staker]
-
-		stakerData.Stake += amount
-
-		poolStorage.TotalStaked += amount
-
-		poolStorage.Stakers[staker] = stakerData
-
-		requiredStake := globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.NetworkParameters.ValidatorStake
-
-		if poolStorage.TotalStaked >= requiredStake {
-
-			if !slices.Contains(globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey) {
-
-				globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry = append(
-					globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey,
-				)
+				return false
 
 			}
 
+			if _, exists := poolStorage.Stakers[staker]; !exists {
+
+				poolStorage.Stakers[staker] = structures.Staker{
+
+					Stake: 0,
+				}
+
+			}
+
+			stakerData := poolStorage.Stakers[staker]
+
+			stakerData.Stake += amount
+
+			poolStorage.TotalStaked += amount
+
+			poolStorage.Stakers[staker] = stakerData
+
+			requiredStake := globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.NetworkParameters.ValidatorStake
+
+			if poolStorage.TotalStaked >= requiredStake {
+
+				if !slices.Contains(globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey) {
+
+					globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry = append(
+						globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey,
+					)
+
+				}
+
+			}
+
+			return true
+
 		}
 
-		return true
+		return false
+
+	} else {
+
+		poolStorage := common_functions.GetPoolFromExecThreadState(poolPubKey + "(POOL)_STORAGE_POOL")
+
+		if poolStorage != nil {
+
+			minStake := globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.NetworkParameters.MinimalStakePerEntity
+
+			if amount < minStake {
+
+				return false
+
+			}
+
+			if _, exists := poolStorage.Stakers[staker]; !exists {
+
+				poolStorage.Stakers[staker] = structures.Staker{
+
+					Stake: 0,
+				}
+
+			}
+
+			stakerData := poolStorage.Stakers[staker]
+
+			stakerData.Stake += amount
+
+			poolStorage.TotalStaked += amount
+
+			poolStorage.Stakers[staker] = stakerData
+
+			requiredStake := globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.NetworkParameters.ValidatorStake
+
+			if poolStorage.TotalStaked >= requiredStake {
+
+				if !slices.Contains(globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey) {
+
+					globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry = append(
+						globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey,
+					)
+
+				}
+
+			}
+
+			return true
+
+		}
+
+		return false
 
 	}
-
-	return false
 
 }
 
@@ -244,50 +299,102 @@ func Unstake(delayedTransaction map[string]string, context string) bool {
 
 	}
 
-	poolStorage := common_functions.GetFromApprovementThreadState(poolPubKey + "(POOL)_STORAGE_POOL")
+	if context == "AT" {
 
-	if poolStorage != nil {
+		poolStorage := common_functions.GetFromApprovementThreadState(poolPubKey + "(POOL)_STORAGE_POOL")
 
-		stakerData, exists := poolStorage.Stakers[unstaker]
+		if poolStorage != nil {
 
-		if !exists {
+			stakerData, exists := poolStorage.Stakers[unstaker]
 
-			return false
+			if !exists {
+
+				return false
+
+			}
+
+			if stakerData.Stake < amount {
+
+				return false
+
+			}
+
+			stakerData.Stake -= amount
+
+			poolStorage.TotalStaked -= amount
+
+			if stakerData.Stake == 0 {
+
+				delete(poolStorage.Stakers, unstaker) // no sense to store staker with 0 balance in stakers list
+
+			} else {
+
+				poolStorage.Stakers[unstaker] = stakerData
+
+			}
+
+			requiredStake := globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.NetworkParameters.ValidatorStake
+
+			if poolStorage.TotalStaked < requiredStake {
+
+				removeFromSlice(globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey)
+
+			}
+
+			return true
 
 		}
 
-		if stakerData.Stake < amount {
+		return false
 
-			return false
+	} else {
+
+		poolStorage := common_functions.GetPoolFromExecThreadState(poolPubKey + "(POOL)_STORAGE_POOL")
+
+		if poolStorage != nil {
+
+			stakerData, exists := poolStorage.Stakers[unstaker]
+
+			if !exists {
+
+				return false
+
+			}
+
+			if stakerData.Stake < amount {
+
+				return false
+
+			}
+
+			stakerData.Stake -= amount
+
+			poolStorage.TotalStaked -= amount
+
+			if stakerData.Stake == 0 {
+
+				delete(poolStorage.Stakers, unstaker) // no sense to store staker with 0 balance in stakers list
+
+			} else {
+
+				poolStorage.Stakers[unstaker] = stakerData
+
+			}
+
+			requiredStake := globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.NetworkParameters.ValidatorStake
+
+			if poolStorage.TotalStaked < requiredStake {
+
+				removeFromSlice(globals.EXECUTION_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey)
+
+			}
+
+			return true
 
 		}
 
-		stakerData.Stake -= amount
-
-		poolStorage.TotalStaked -= amount
-
-		if stakerData.Stake == 0 {
-
-			delete(poolStorage.Stakers, unstaker) // no sense to store staker with 0 balance in stakers list
-
-		} else {
-
-			poolStorage.Stakers[unstaker] = stakerData
-
-		}
-
-		requiredStake := globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.NetworkParameters.ValidatorStake
-
-		if poolStorage.TotalStaked < requiredStake {
-
-			removeFromSlice(globals.APPROVEMENT_THREAD_METADATA_HANDLER.Handler.EpochDataHandler.PoolsRegistry, poolPubKey)
-
-		}
-
-		return true
+		return false
 
 	}
-
-	return false
 
 }
