c0c6f9ddf745c24eca82f1d278b786079b53b40d
VladChernenko <vladchernenko@protonmail.com>
2025-09-12 03:08:49 +0200

    Build scripts update to let it run on Linux/Windows/Mac

--- build.bat
+++ build.bat
@@ -3,10 +3,7 @@ setlocal
 echo Downloading dependencies...
 go mod download
 
-:: Build the project
 echo Building the project...
-set GOOS=windows
-set GOARCH=amd64
 go build -o modulr.exe
 
 echo Done. Binary name is: modulr.exe
--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
+TARGET_OS=${1:-$(go env GOOS)}
+TARGET_ARCH=${2:-$(go env GOARCH)}
+
 echo "Downloading dependencies..."
 go mod download
 
-# Build the project
-echo "Building the project..."
-GOOS=linux GOARCH=amd64 go build -o modulr
+echo "Building the project for $TARGET_OS/$TARGET_ARCH..."
+GOOS=$TARGET_OS GOARCH=$TARGET_ARCH go build -o modulr
 
-echo "Done. Binary name is : modulr"
+echo "Done. Binary name is: modulr"
