7a46e03bad62552f6770f1ca0c17062fa89f61e0
VladChernenko <vladchernenko@protonmail.com>
2025-12-07 05:23:37 +0100

    Added structures to interact with anchors

--- threads/sequence_alingment_copy.go
+++ threads/sequence_alingment_copy.go
@@ -1,5 +1,21 @@
 package threads
 
+import (
+	"github.com/modulrcloud/modulr-core/anchors_pack"
+	"github.com/modulrcloud/modulr-core/structures"
+)
+
+type SequenceAlignmentAnchorData struct {
+	AggregatedAnchorRotationProof anchors_pack.AggregatedAnchorRotationProof `json:"aarp"`
+	FoundInBlock                  int                                        `json:"foundInBlock"`
+}
+
+type SequenceAlignmentDataResponse struct {
+	FoundInAnchorIndex int                                     `json:"foundInAnchorIndex"`
+	Anchors            map[int]SequenceAlignmentAnchorData     `json:"anchors"`
+	Afp                *structures.AggregatedFinalizationProof `json:"afp,omitempty"`
+}
+
 func SequenceAlignmentThread2() {
 
 	// Stub - TBD
