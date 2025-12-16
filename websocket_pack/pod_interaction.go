49de3ebed852ca78494e5338c7afef3fcfb0e789
Vlad <53381472+VladChernenko@users.noreply.github.com>
2025-12-16 17:14:33 +0100

    Add PoD fallback for leader finalization proofs

--- websocket_pack/pod_interaction.go
+++ websocket_pack/pod_interaction.go
@@ -14,3 +14,23 @@ func SendBlockAndAfpToPoD(block block_pack.Block, afp structures.AggregatedFinal
 		_, _ = utils.SendWebsocketMessageToPoD(reqBytes)
 	}
 }
+
+func SendAggregatedLeaderFinalizationProofToPoD(proof structures.AggregatedLeaderFinalizationProof) {
+	req := WsAggregatedLeaderFinalizationProofStoreRequest{Route: "accept_aggregated_leader_finalization_proof", Proof: proof}
+	if reqBytes, err := json.Marshal(req); err == nil {
+		_, _ = utils.SendWebsocketMessageToPoD(reqBytes)
+	}
+}
+
+func GetAggregatedLeaderFinalizationProofFromPoD(epochIndex int, leader string) *structures.AggregatedLeaderFinalizationProof {
+	req := WsAggregatedLeaderFinalizationProofRequest{Route: "get_aggregated_leader_finalization_proof", EpochIndex: epochIndex, Leader: leader}
+	if reqBytes, err := json.Marshal(req); err == nil {
+		if respBytes, err := utils.SendWebsocketMessageToPoD(reqBytes); err == nil {
+			var resp WsAggregatedLeaderFinalizationProofResponse
+			if err := json.Unmarshal(respBytes, &resp); err == nil {
+				return resp.Proof
+			}
+		}
+	}
+	return nil
+}
